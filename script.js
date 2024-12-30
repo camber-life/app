62f5bf8d311a449fc0aae0eccb3b81e9e588b4f3
camber-life <dev@camber.life>
2024-12-29 22:58:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Nav Toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
